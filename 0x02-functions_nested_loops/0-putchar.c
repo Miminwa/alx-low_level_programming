2a35dd7288dfff6868d25dc328e76a4ff5cadd84
Miracle <bchimamanda@gmail.com>
2022-07-05 19:51:35 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar
+ *
+ * created : Miminwa
+ * cc: 5 july 2022
+ *
+ * Return: (0) if successful
+ */
+
+int main(void)
+{
+	char va[] = "_putchar";
+	int ii;
+	/* length of the array var*/
+	int lenVar = sizeof(var) / sizeof(var[0]);
+
+	for (ii = 0; ii < lenVar - 1; ii++)
+	{
+		_putchar(var[ii]);
+	}
+	       _puchar('\n');
+
+	return (o);
+}
