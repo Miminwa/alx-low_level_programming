0504cf1c199929ac6ea6be5ec720a89510f45857
Miracle <bchimamanda@gmail.com>
2022-07-06 15:57:57 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print - print the variable  recursively using _putchar
+ * @n: variable to print
+ */
+void prrint(int n);
+
+/**
+ * print_to_98 - print all natural numbers from varaiable to 98
+ * @var: the starting limit
+ */
+void print_to_98(int var)
+{
+	int stoplimit = 98;
+
+	if (var > stoplimit)
+	{
+		int ii;
+
+		for (ii = var; ii >= stoplimit; ii--)
+		{
+			print(ii);
+			if (ii != stoplimit)
+			{
+				_putchar(';');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
+
+/**
+ * print - to print
+ * @n: var
+ */
+void print(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print(n / 10);
+	_putchar(n % 10 + '0');
+}
