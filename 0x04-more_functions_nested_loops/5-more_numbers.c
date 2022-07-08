741b80fb47db60a9954737d4476ea97c69d28d2b
Miracle <bchimamanda@gmail.com>
2022-07-07 18:47:33 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,16 @@
 
 void more_numbers(void)
 {
-	char i;
-	int counter;
+	int x, y;
 
-	for (counter = 1; counter <= 10; counter++)
+	for (x = 0; x < 10; x++)
 	{
-		for (i = 0 ; i <= 14; i++)
+		for (y = 0; y < 15; y++)
 		{
-			if (i / 10 > 0)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-		} 
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
